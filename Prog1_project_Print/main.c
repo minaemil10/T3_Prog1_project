3ec2722bb59ddbd43e2a2bbb4ee5e0c7276d79e2
minaemil10 <minaemile3@gmail.com>
2023-12-23 21:42:08 +0200

    Print(sort) is finished and tested with dummy data
    fn is 100% done as far as needed logic go
    
    errors and crashes still need to be tested

--- Prog1_project_Print/main.c
+++ Prog1_project_Print/main.c
@@ -22,7 +22,7 @@ typedef struct
 } user;
 
 user accounts[MAX_ACCOUNTS];        //accounts data
-int count = 5;
+int count = 6;
 void declare()
 {
     //0
@@ -56,7 +56,7 @@ void declare()
     accounts[3].balance = 123.8;
     strcpy(accounts[3].mobile,"01554698321") ;
     accounts[3].d_open.month = 6;
-    accounts[3].d_open.year = 2011;
+    accounts[3].d_open.year = 2010;
     //4
     accounts[4].account_no = 9780131619;
     strcpy(accounts[4].name,"Fady Mohamed") ;
@@ -65,6 +65,14 @@ void declare()
     strcpy(accounts[4].mobile,"03254698321") ;
     accounts[4].d_open.month = 3;
     accounts[4].d_open.year = 2012;
+    //5
+    accounts[5].account_no = 9780131111;
+    strcpy(accounts[5].name,"Fady Young") ;
+    strcpy(accounts[5].mail,"f.young@gmail.com") ;
+    accounts[5].balance = 0.99;
+    strcpy(accounts[5].mobile,"01254961321") ;
+    accounts[5].d_open.month = 12;
+    accounts[5].d_open.year = 2008;
 }
 const char* monthName(int month)
 {
@@ -119,7 +127,37 @@ void printer(user a)
     printf("Date Opened: %s %d\n",monthName(a.d_open.month),a.d_open.year);
 }
 
-void sortByName(user *a)
+void sortByName(user *a, int z)
+{
+    int i,j, flag = 1,type;
+    if(z == 1)
+    {
+        type = 1;
+    }
+    else
+    {
+        type = -1;
+    }
+    user x;
+    for(i = 0 ; i < count-1; i++)
+    {
+        if(flag == 0) break;
+        flag = 0;
+        for(j = 0 ; j < count - i -1 ; j++)
+        {
+
+            if(strcmp((a+ j)-> name, (a+ j +1)-> name ) == type)
+            {
+                x =  (*(a+ j));
+                (*(a + j)) = (*(a+ j +1));
+                (*(a+ j +1)) = x;
+                flag = 1;
+            }
+        }
+    }
+}
+
+void sortByBalance(user *a,int z)
 {
     int i,j, flag = 1;
     user x;
@@ -129,8 +167,65 @@ void sortByName(user *a)
         flag = 0;
         for(j = 0 ; j < count - i -1 ; j++)
         {
+            if(z == 1)
+            {
 
-            if(strcmp((a+ j)-> name , (a+ j +1)-> name ) == 1)
+
+                if((a+ j)-> balance < (a+ j +1)-> balance)
+                {
+                    x =  (*(a+ j));
+                    (*(a + j)) = (*(a+ j +1));
+                    (*(a+ j +1)) = x;
+                    flag = 1;
+                }
+            }
+            else
+            {
+                if((a+ j)-> balance > (a+ j +1)-> balance)
+                {
+                    x =  (*(a+ j));
+                    (*(a + j)) = (*(a+ j +1));
+                    (*(a+ j +1)) = x;
+                    flag = 1;
+                }
+            }
+        }
+    }
+}
+
+int dateCmp(date a, date b)
+{
+    if(a.year > b.year ) return 1;
+    if(a.year < b.year ) return -1;
+    if(a.year == b.year)
+    {
+        if(a.month > b.month ) return 1;
+        if(a.month < b.month ) return -1;
+        if(a.month == b.month ) return 0;
+    }
+
+}
+
+void sortByDate(user *a,int z)
+{
+    int i,j, flag = 1,type;
+    if(z == 1)
+    {
+        type = 1;
+    }
+    else
+    {
+        type = -1;
+    }
+    user x;
+    for(i = 0 ; i < count-1; i++)
+    {
+        if(flag == 0) break;
+        flag = 0;
+        for(j = 0 ; j < count - i -1 ; j++)
+        {
+
+            if(dateCmp((a+ j)-> d_open,(a+ j +1)-> d_open) == type)
             {
                 x =  (*(a+ j));
                 (*(a + j)) = (*(a+ j +1));
@@ -139,31 +234,69 @@ void sortByName(user *a)
             }
         }
     }
-
-    //printf("%s\n",(a +1)->name);
-    //strcpy((a)->name,(a +1)->name);
 }
+
 void print()
 {
     user sorted[MAX_ACCOUNTS] ;
-    int i;
-    printf("Not sorted:\n");
+    int i,way,type;
     for(i=0 ; i < count ; i++)
     {
-        sorted[i] = accounts[i];
-
-        printer(accounts[i]);
-        printf("\n");
+        sorted[i] = accounts[i]; //array to change it,s order without changing the original array
     }
+    printf("how would you like the accounts to be sorted?\n");
+    printf("1) By name\n2) By Balance\n3) By date opened\n");
+    do
+    {
+        scanf("%d",&way);
+        if(!(way>0 && way<4)) printf("Please enter number from 1 to 3!\n");
+    }
+    while(!(way>0 && way<4));
+    switch(way)
+    {
+    case 1:
+        printf("Please choose the type:\n");
+        printf("1) A-Z\n2) Z-A\n");
+        do
+        {
+            scanf("%d",&type);
+            if(!(type>0 && type<3)) printf("Please enter number from 1 to 2!\n");
+        }
+        while(!(type>0 && type<3));
+        sortByName(&sorted,type);
+        break;
+    case 2:
+        printf("Please choose the type:\n");
+        printf("1) highest to lowest\n2) lowest to highest\n");
+        do
+        {
+            scanf("%d",&type);
+            if(!(type>0 && type<3)) printf("Please enter number from 1 to 2!\n");
+        }
+        while(!(type>0 && type<3));
+        sortByBalance(&sorted,type);
+        break;
+    case 3:
+        printf("Please choose the type:\n");
+        printf("1) old to new\n2) new to old\n");
+        do
+        {
+            scanf("%d",&type);
+            if(!(type>0 && type<3)) printf("Please enter number from 1 to 2!\n");
+        }
+        while(!(type>0 && type<3));
+        sortByDate(&sorted,type);
+        break;
 
-    sortByName(&sorted);
+
+    }
     printf("\n\n");
-    printf("sorted:\n");
-    for(i=0 ; i < count ; i++)
+    for(i=0 ; i < count ; i++) //print sorted array
     {
         printer(sorted[i]);
         printf("\n");
     }
+
 }
 int main()
 {
