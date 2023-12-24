0d15bfb88743f121508bd0db93c38f12158f7cb8
minaemil10 <minaemile3@gmail.com>
2023-12-24 21:16:59 +0200

    implemented load function
    opened a new project for advanced search
    
    implemented Rober's load function in my print(sort) function and it worked exceptionally well

--- Prog1_project_Print/main.c
+++ Prog1_project_Print/main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAX_USERNAME_LENGTH 50
 #define MAX_PASSWORD_LENGTH 50
 #define MAX_ACCOUNTS        100
+
 typedef struct
 {
     int month;
@@ -13,7 +13,7 @@ typedef struct
 } date;
 typedef struct
 {
-    unsigned long long int account_no;/*the data size is large*/
+    char account_no[12];
     char name[100];
     char mail[100];
     double balance;
@@ -22,11 +22,120 @@ typedef struct
 } user;
 
 user accounts[MAX_ACCOUNTS];        //accounts data
-int count = 6;
+int count = 0;                      //accounts number
+
+void loadAccounts()
+{
+    const char* filename="accounts.txt";
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error opening file: %s\n", filename);
+        return 0;
+    }
+
+
+    char line[400];
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        char* token;
+        token = strtok(line, ",");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        strcpy(accounts[count].account_no,token);
+
+        token = strtok(NULL, ",");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        strcpy(accounts[count].name, token);
+
+        token = strtok(NULL, ",");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        strcpy(accounts[count].mail, token);
+
+        token = strtok(NULL, ",");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        accounts[count].balance = atof(token);
+
+        token = strtok(NULL, ",");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        strcpy(accounts[count].mobile, token);
+
+        token = strtok(NULL, "-");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+
+        accounts[count].d_open.month=atoi(token);
+        token = strtok(NULL, " ");
+        if (token == NULL)
+        {
+            printf("Invalid file format: %s\n", filename);
+            break;
+        }
+        accounts[count].d_open.year = atoi(token);
+        count++;
+        if (count >= MAX_ACCOUNTS)
+        {
+            printf("Maximum account limit reached: %d\n", MAX_ACCOUNTS);
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+void save(){
+    const char* filename="accounts.txt";
+    int i ;
+    FILE* file = fopen(filename, "w+");
+    if (file == NULL)
+    {
+        printf("Error opening file: %s\n", filename);
+        return 0;
+    }
+
+    for(i=0;i<count;i++){
+
+        fprintf(file,"%s,%s,%s,%0.2f,%s,%d-%d \n",accounts[i].account_no,accounts[i].name,accounts[i].mail,accounts[i].balance,accounts[i].mobile,accounts[i].d_open.month,accounts[i].d_open.year);
+
+    }
+    printf("\nsaved succesfully\n");
+    fclose(file);
+}
+void quit(){
+    printf("Are you sure thet you want to exit?\n1)yes                  2)no\n");
+    int val;
+    scanf("%d",&val);
+    if(val==1)
+        exit(1);
+}
+
+
 void declare()
 {
     //0
-    accounts[0].account_no = 9780136019;
+    strcpy(accounts[0].account_no,"9780136019");
     strcpy(accounts[0].name,"Mohamed Ali") ;
     strcpy(accounts[0].mail,"m.ali@gmail.com") ;
     accounts[0].balance = 10000.54;
@@ -34,7 +143,7 @@ void declare()
     accounts[0].d_open.month = 12;
     accounts[0].d_open.year = 2008;
     //1
-    accounts[1].account_no = 9780135102;
+    strcpy(accounts[1].account_no,"9780135102");
     strcpy(accounts[1].name,"Ahmed Ashraf") ;
     strcpy(accounts[1].mail,"a.ashraf@gmail.com") ;
     accounts[1].balance = 800.93;
@@ -42,7 +151,7 @@ void declare()
     accounts[1].d_open.month = 10;
     accounts[1].d_open.year = 2009;
     //2
-    accounts[2].account_no = 9780136498;
+    strcpy(accounts[2].account_no,"9780136498");
     strcpy(accounts[2].name,"Belal Ahmed") ;
     strcpy(accounts[2].mail,"b.ahmed@gmail.com") ;
     accounts[2].balance = 50000.16;
@@ -50,7 +159,7 @@ void declare()
     accounts[2].d_open.month = 8;
     accounts[2].d_open.year = 2010;
     //3
-    accounts[3].account_no = 9780138871;
+    strcpy(accounts[3].account_no,"9780138871");
     strcpy(accounts[3].name,"Chris Ali") ;
     strcpy(accounts[3].mail,"c.ali@gmail.com") ;
     accounts[3].balance = 123.8;
@@ -58,7 +167,7 @@ void declare()
     accounts[3].d_open.month = 6;
     accounts[3].d_open.year = 2010;
     //4
-    accounts[4].account_no = 9780131619;
+    strcpy(accounts[4].account_no,"9780131619");
     strcpy(accounts[4].name,"Fady Mohamed") ;
     strcpy(accounts[4].mail,"f.mohamed@gmail.com") ;
     accounts[4].balance = 6922.47;
@@ -66,7 +175,7 @@ void declare()
     accounts[4].d_open.month = 3;
     accounts[4].d_open.year = 2012;
     //5
-    accounts[5].account_no = 9780131111;
+    strcpy(accounts[5].account_no,"9780131111");
     strcpy(accounts[5].name,"Fady Young") ;
     strcpy(accounts[5].mail,"f.young@gmail.com") ;
     accounts[5].balance = 0.99;
@@ -119,7 +228,7 @@ const char* monthName(int month)
 }
 void printer(user a)
 {
-    printf("Account number: %llu\n",a.account_no);
+    printf("Account number: %s\n",a.account_no);
     printf("Name: %s\n",a.name);
     printf("E-mail: %s\n",a.mail);
     printf("Balance: %.2lf $\n",a.balance);
@@ -300,7 +409,10 @@ void print()
 }
 int main()
 {
-    declare();
+    //declare();
+    loadAccounts();
     print();
+    save();
+    quit();
     return 0;
 }
